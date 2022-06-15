fe717fd8cd9d232ff1a4bd186314f5c13ac71ee7
Mergestack Mac <mergestackmac@Mergestacks-MacBook-Pro.local>
2022-06-15 18:30:11 +0500

    Moved to typescript and created basic folder for my project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+<div className="App">
+<Helmet>
+          <meta charSet="utf-8" />
+          <title>Student Portal</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <meta name="author" content="Creating Student Online Portal" />
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.tsx</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+</div>
+    
   );
 }
 
