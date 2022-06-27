d17d70294a3771654f1c9f25385d015b9758f2e1
AYAZ <106649385+HMayaz001@users.noreply.github.com>
2022-06-27 12:33:19 +0500

    Update src/utils/helper.tsx
    Co-authored-by: Muhammad Ahmad Faraz <ahmadfaraz629@gmail.com>

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export function dateFormat(date: any) {
+export function dateFormat(date: Date | String) {
   return moment(date).format('MMMM Do YYYY, h:mm:ss a');
 }
